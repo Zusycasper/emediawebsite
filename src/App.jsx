bf852d8b236fe8aaf07365a7cd269264a0f2269c
Zusy Casper <zusycasper@gmail.com>
2025-09-24 20:27:08 +0530

    removed unwanted router paths

--- src/App.jsx
+++ src/App.jsx
@@ -21,11 +21,6 @@ function App() {
   <Route path="/about" element={<AboutUs />} />
   <Route path="/contact" element={<ContactUs />} />
   <Route path="/services" element={<Services />} />
-  {/* <Route path="/digital_marketing" element={<DigiMarketing />} />
-  <Route path="/web_development" element={<WebDevelopment />} />
-  <Route path="/social_media_management" element={<SocialMediaManagement />} />
-  <Route path="/cloud_infrastructure_support" element={<CloudITInfrastructureSupport/> } />
-  <Route path="/creative_design" element={<CreativeDesign />} /> */}
 </Routes>
 
   {/* <Footer /> */}
