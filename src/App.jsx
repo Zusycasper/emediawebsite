56897fd625fcaa100b3383eeb3d3452c5e6a2804
Zusy Casper <zusycasper@gmail.com>
2025-09-24 14:14:09 +0530

    route fixed for new 3 services page

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,9 @@ import WebDevelopment from './pages/WebDevelopment/WebDevelopment';
 import ContactUs from './pages/ContactUs/ContactUs';
 import Footer from './components/Footer/Footer';
 import Preloader from "./pages/Preloder/Preloader";
+import SocialMediaManagement from "./pages/SocialMediaManagement/SocialMediaManagement";
+import CloudITInfrastructureSupport from "./pages/CloudITInfrastructureSupport/CloudITInfrastructureSupport";
+import CreativeDesign from "./pages/CreativeDesign/CreativeDesign";
 
 function App() {
   return (
@@ -20,6 +23,9 @@ function App() {
   <Route path="/services" element={<Services />} />
   <Route path="/digital_marketing" element={<DigiMarketing />} />
   <Route path="/web_development" element={<WebDevelopment />} />
+  <Route path="/social_media_management" element={<SocialMediaManagement />} />
+  <Route path="/cloud_infrastructure_support" element={<CloudITInfrastructureSupport/> } />
+  <Route path="/creative_design" element={<CreativeDesign />} />
 </Routes>
 
   {/* <Footer /> */}
