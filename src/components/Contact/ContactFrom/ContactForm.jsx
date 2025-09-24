aa11236330e084fd2e5588b8aa7a538081aa8e5b
Zusy Casper <zusycasper@gmail.com>
2025-09-24 20:00:10 +0530

    made changes to to commented sections

--- src/components/Contact/ContactFrom/ContactForm.jsx
+++ src/components/Contact/ContactFrom/ContactForm.jsx
@@ -21,14 +21,6 @@ function ContactForm() {
                 Got a question? Fill out the form below, and one of our team
                 members will get back to you soon.
               </p>
-
-              {/* <p className="text-gray-600 text-lg font-bold leading-relaxed mb-1">
-                Need to talk with an agent?
-              </p>
-              <p className="text-gray-600 text-lg font-light leading-relaxed">
-                No worries. You can email us directly at one of our regional
-                offices; one of our agents will get back to you soon
-              </p> */}
             </div>
 
             <div className="w-[full]">
