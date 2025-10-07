778eb0eda5dbb8c74b0c2cf761a8fa7c553d406e
Zusy Casper <zusycasper@gmail.com>
2025-10-07 13:49:20 +0530

    added new addressses

--- src/components/Contact/AddressBanner/AddressBanner.jsx
+++ src/components/Contact/AddressBanner/AddressBanner.jsx
@@ -4,15 +4,15 @@ function AddressBanner() {
   return (
     <div>
       <section className="py-10 bg-gradient-to-r from-teal-500 to-pink-500 overflow-hidden w-full">
-        <div className="max-w-screen-xl mx-auto px-4">
+        <div className="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="text-center">
-            <div className="flex flex-col md:flex-row items-center justify-center md:space-x-10 text-white text-lg md:text-2xl font-bold space-y-6 md:space-y-0">
+            <div className="flex flex-col md:flex-row items-center justify-center md:space-x-40 text-white text-lg md:text-2xl font-bold space-y-6 md:space-y-0">
               
               {/* UK */}
               <span>
-                <h3>UK</h3>
-                <h6 className='font-medium'>London</h6>
-                <p className='font-light'>10 Baker Street, London, NW1 6XE</p>
+                <h3>United Kingdom (UK)</h3>
+                <h6 className='font-medium'>Watford</h6>
+                <p className='font-light'></p>
               </span>
 
               {/* Separator */}
@@ -21,8 +21,7 @@ function AddressBanner() {
               {/* Sri Lanka */}
               <span>
                 <h3>Sri Lanka (SL)</h3>
-                <h6 className='font-medium'>Colombo</h6>
-                <p className='font-light'>45 Marine Drive, Colombo 03</p>
+                <h6 className='font-medium'>Biyagama</h6>
               </span>
 
               {/* Separator */}
@@ -31,8 +30,8 @@ function AddressBanner() {
               {/* Dubai */}
               <span>
                 <h3>Dubai</h3>
-                <h6 className='font-medium'>Downtown Dubai</h6>
-                <p className='font-light'>Tower 3, Sheikh Zayed Road, Dubai</p>
+                {/* <h6 className='font-medium text-tr/ansparent'>Downtown Dubai</h6> */}
+                {/* // <p className='font-light'>Tower 3, Sheikh Zayed Road, Dubai</p> */}
               </span>
 
             </div>
